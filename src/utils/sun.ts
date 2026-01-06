5e7506b86c5fed7daa7758e350e42dd02f5e0799
Ivan Tikhonov <ivan.tikhonov.spb@gmail.com>
2026-01-06 16:16:24 +0300

    Fix sun times corner cases

--- src/utils/sun.ts
+++ src/utils/sun.ts
@@ -3,6 +3,13 @@ import * as SunCalc from 'suncalc';
 export function getDayLengthInHours(date: Date, latitude: number, longitude: number): number {
     const times = SunCalc.getTimes(date, latitude, longitude);
     const dayLengthMs = times.sunset.getTime() - times.sunrise.getTime();
+    if (!dayLengthMs) {
+        if (SunCalc.getPosition(times.solarNoon, latitude, longitude).altitude > 0) {
+            return 24;
+        } else {
+            return 0;
+        }
+    }
     return dayLengthMs / (1000 * 60 * 60);
 }
 
